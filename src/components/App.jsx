40c722d3972c6aef3e8e5f85c8b2d66ddaadddb8
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-06-20 22:17:50 +0300

    2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,6 @@
+import user from "./user/user.json";
+import Profile from "./user/profile";
+
 export const App = () => {
   return (
     <div
@@ -10,7 +13,15 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+        />
     </div>
   );
 };
