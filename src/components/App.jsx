e7e1675bb76308e3ac04261e0b6b0478a9bcd896
Aleksandr Zhadanov <74437337+jadanov89@users.noreply.github.com>
2023-06-22 00:14:38 +0300

    3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,11 @@
 import user from "./user/user.json";
 import Profile from "./user/profile";
+import data from "./data/data.json";
+import Statistics from "./data/statistics";
+import friends from "./friends/friends.json";
+import FriendList from "./friends/friendlist";
+import TransactionHistory from "./transactionHistory/transactionhistory";
+import transactions from "./transactionHistory/transactions.json";
 
 export const App = () => {
   return (
@@ -22,6 +28,9 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
         />
+        <Statistics title="Upload stats" stats={data} />
+        <FriendList friends={friends} />
+        <TransactionHistory transaction={transactions} />
     </div>
   );
 };
---
+++ src/components/data/statistics.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import styles from './statistical.module.css';
+
+const Statistics = (
+    {
+        title, stats 
+    }
+) => {
+    return (
+        <section className={styles.statistics}>
+            {title && <h2 className={styles.title}>{title}</h2>}
+
+            <ul className={styles.stat_list}>
+                {stats.map(stat => (
+                    <li className={styles.item} key={stat.id}>
+                        <span className={styles.label}>{stat.label}</span>
+                        <span className={styles.percentage}>{stat.percentage}</span>
+                    </li>
+                ))}
+            </ul>
+        </section>
+    );
+}
+
+Statistics.propTypes = {
+    title: PropTypes.string,
+    stats: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        label: PropTypes.string,
+        percentage: PropTypes.number,
+      })
+    ).isRequired,
+  };
+
+
+export default Statistics;
